2ba5e35bf358bcb9806061aca3d05958e4e65567
sean1211 <medina1211@gmail.com>
2015-12-13 14:37:37 -0500

    plot2

---
+++ plot2.r
@@ -0,0 +1,8 @@
+plot2 <- function() {
+  plot(df$timestamp,df$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+ 
+   dev.copy(png, file="plot2.png", width=480, height=480)
+ 
+  cat("plot2.png has been saved in", getwd())
+}
+plot2()
