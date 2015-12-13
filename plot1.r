f89860449d86a56ff1c8425192a87c7a3810bdd0
sean1211 <medina1211@gmail.com>
2015-12-13 16:07:02 -0500

    plot1

--- plot1.r
+++ plot1.r
@@ -1,9 +1,13 @@
-plot1 <- function() {
-  hist(df$Global_active_power, main = paste("Global Active Power"), col="red", xlab="Global Active Power (kilowatts)")
-  
-  dev.copy(png, file="plot1.png", width=480, height=480)
-  
-  cat("Plot1.png is saved in", getwd())
-}
-plot1()
+dataSet <- "household_power_consumption.txt"
+data <- read.table(dataSet, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+Sample<- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+datetime <- strptime(paste(Sample$Date, Sample$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+
+GAP <- as.numeric(as.character(Sample$Global_active_power))
+
+
+hist(GAP, main = paste("Global Active Power"), col="red", xlab="Global Active Power (kilowatts)")
+
+dev.copy(png, file="plot1.png", width=480, height=480)
+dev.off()
 
