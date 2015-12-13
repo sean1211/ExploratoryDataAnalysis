69d251ae33d4b9013644699bcc20a5c04ccb7b05
sean1211 <medina1211@gmail.com>
2015-12-13 13:47:13 -0500

    plot1

---
+++ plot1.r
@@ -0,0 +1,9 @@
+plot1 <- function() {
+  hist(df$Global_active_power, main = paste("Global Active Power"), col="red", xlab="Global Active Power (kilowatts)")
+  
+  dev.copy(png, file="plot1.png", width=480, height=480)
+  
+  cat("Plot1.png is saved in", getwd())
+}
+plot1()
+
